4ed6851c8b29da8961458b9fc8422f17ed0461fd
Steven <steven19930326@gmail.com>
2022-03-16 16:27:38 +0800

    Customed for Project

---
+++ Collection/PHP/4.edit-api.php
@@ -0,0 +1,56 @@
+<?php
+require __DIR__ . '../parts/connect_db.php';
+
+header('Content-Type: application/json');
+// 輸出的資料格式
+$output = [
+    'success' => false,
+    'error' => '沒有表單資料',
+    'code' => 0,
+    'postData' => [],
+    'rowCount' => 0,
+];
+
+$output['postData'] = $_POST;  // 讓前端做資料查看,資料是否一致
+
+if(empty($_POST['sid']) or empty($_POST['Nickname'])){
+    echo json_encode($output, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+
+
+// TODO: 欄位檢查
+
+
+$sql = "UPDATE `address_book` SET  
+        `Nickname`=?,
+        `Project Name`=?,
+        `Discription`=?,
+        `Tags`=?,
+        `address`=?
+        WHERE `sid`=?";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+    $_POST['Nickname'],
+    $_POST['Project Name'] ?? '',
+    $_POST['Discription'] ?? '',
+    $_POST['Tags'] ?? null,
+    $_POST['address'] ?? '',
+    $_POST['sid'],
+]);
+
+
+$output['rowCount'] = $stmt->rowCount(); // 修改資料的筆數
+if($stmt->rowCount()){
+    $output['error'] = '';
+    $output['success'] = true;
+} else {
+    $output['error'] = '資料沒有修改';
+}
+
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
+
