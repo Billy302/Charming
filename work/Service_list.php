1c72ab1569aeb1bcf2cbaffe8ddaee1d87a35921
Billy <a710146@hotmail.com>
2022-03-23 20:19:25 +0800

    畫面調整 &  修改圖示 & 欄位新增排序功能 & 關鍵字搜尋

--- work/Service_list.php
+++ work/Service_list.php
@@ -4,10 +4,14 @@ $title = '服務項目總覽';
 $pageName = 'Service_list';
 $perPage = 10; // 每頁有幾筆
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1; //用戶要看的頁碼
+$search = isset($_GET['search']) ? "service_ltems LIKE '%" . $_GET['search'] . "%' " : 1;
+$select = isset($_GET['select']) ? $_GET['select'] : 'ID';
+$sort = isset($_GET['sort']) ? $_GET['sort'] : 'ASC';
+
 
 $t_sql = "SELECT count(1) FROM `work_service` ";
 if ($page < 1) {
-    header('Location: Service-list.php?page=1');
+    header("Location: Service_list.php?page=1&select=$select&sort=$sort");
     exit;
 }
 
@@ -15,61 +19,157 @@ $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
 $rows = [];
 $totalPages = 0;
 
+
 if ($totalRows) {
     // 總頁數
     $totalPages = ceil($totalRows / $perPage);
     if ($page > $totalPages) {
-        header("Location: ab-list.php?page=$totalpages");
+        header("Location: ab-list.php?page=$totalpages&select=$select&sort=$sort");
         exit;
     }
-    $sql = sprintf("select ID, title, service_ltems, service_hours, service_fee, production_time, modifications, user_ID, create_at FROM work_service order by ID  limit %s ,%s", ($page - 1) * $perPage, $perPage);
+    $sql = sprintf("select ID, title, service_ltems, service_hours, service_fee, production_time, modifications, user_ID, create_at FROM work_service where %s order by %s %s limit %s ,%s", $search, $select, $sort, ($page - 1) * $perPage, $perPage);
+    // $sql = sprintf("select ID, title, service_ltems, service_hours, service_fee, production_time, modifications, user_ID, create_at FROM work_service order by ID  limit %s ,%s", ($page - 1) * $perPage, $perPage);  
     // 拿到分頁資料
     $rows = $pdo->query($sql)->fetchAll();
 }
 ?>
+<style>
+    .disabled {
+        pointer-events: none;
+    }
 
+    th,
+    td {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+</style>
 <?php include "../parts/html-head.php"; ?>
 
 <div class="container liststyle">
-    <div class="roW">
+    <div class="row">
         <div class="col">
             <nav class="navstyle" aria-label="Page navigation example">
                 <ul class="d-flex pagination">
-                    <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
-                        <a class="page-link" href="?page=<?= $page - 1 ?>">
-                            << </a>
-                    </li>
+                    <li class="page-item <?= $page == 1 ? 'disabled' : '' ?> ">
+                        <a class="pagestyle" href="?page=<?= $page - 1 ?>&select=<?= $select ?>&sort=<?= $sort ?>">
+                            <i class="fas fa-arrow-circle-left text-align-center "></i>
+                        </a>
+                    </li>  
                     <?php for ($i = $page - 5; $i <= $page + 5; $i++) :
                         if ($i >= 1 and $i <= $totalPages) : ?>
-                            <li class="pagestyle <?= $page == $i ? 'active' : '' ?>">
-                                <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                            <li class="page-item <?= $page == $i ? 'active' : '' ?>">
+                                <a class="pagestyle" href="?page=<?= $i ?>&select=<?= $select ?>&sort=<?= $sort ?>"><?= $i ?></a>
                             </li>
                     <?php endif;
                     endfor; ?>
                     <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>">
-                        <a class="page-link" href="?page=<?= $page + 1 ?>">
-                            >>
+                        <a class="pagestyle" href="?page=<?= $page + 1 ?>&select=<?= $select ?>&sort=<?= $sort ?>">
+                            <i class="fas fa-arrow-circle-right"></i>
                         </a>
                     </li>
                 </ul>
             </nav>
         </div>
     </div>
+    <form name="searchbar" method="get" action="Service_list.php">
+        <div class="input-group">
+            <div class="form-outline">
+                <input id="search-focus" type="teax" class="form-control" placeholder="服務項目，關鍵字搜尋" name="search" />
+            </div>
+            <button type="submit" class="newinfo">
+                <i class="fas fa-search"></i>
+            </button>
+        </div>
+    </form>
     <div class="row">
         <div class="col">
             <table class="table table-striped tablestyle">
                 <thead>
                     <tr>
                         <th scope="col">刪除</th>
-                        <th scope="col">服務ID</th>
-                        <th scope="col">使用者ID</th>
-                        <th scope="col">標題</th>
-                        <th scope="col">服務項目</th>
-                        <th scope="col">服務時間</th>
-                        <th scope="col">服務費用</th>
-                        <th scope="col">製作時間</th>
-                        <th scope="col">修改次數</th>
-                        <th scope="col">建立時間</th>
+                        <th scope="col">
+                            服務ID
+                            <?php $select = "ID";
+                            $sort = "ASC" ?>
+                            <a id="sort_ID_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "ID";
+                            $sort = "DESC" ?>
+                            <a id="sort_ID_DESC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
+                        <th scope="col">
+                            使用者ID
+                            <?php $select = "user_ID";
+                            $sort = "ASC" ?>
+                            <a id="sort_user_ID_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "user_ID";
+                            $sort = "DESC" ?>
+                            <a id="sort_user_ID_DESC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
+                        <th scope="col">
+                            標題
+                            <?php $select = "title";
+                            $sort = "ASC" ?>
+                            <a id="sort_title_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "title";
+                            $sort = "DESC" ?>
+                            <a id="sort_title_DESC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
+                        <th scope="col">
+                            服務項目
+                            <?php $select = "service_ltems";
+                            $sort = "ASC" ?>
+                            <a id="sort_service_ltems_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "service_ltems";
+                            $sort = "DESC" ?>
+                            <a id="sort_service_ltems_DESC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
+                        <th scope="col">
+                            服務時間
+                            <?php $select = "service_hours";
+                            $sort = "ASC" ?>
+                            <a id="sort_service_hours_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "service_hours";
+                            $sort = "DESC" ?>
+                            <a id="sort_service_hours_DESC" href="?page=<?= $page ?>&selecst=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
+                        <th scope="col">
+                            服務費用
+                            <?php $select = "service_fee";
+                            $sort = "ASC" ?>
+                            <a id="sort_service_fee_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "service_fee";
+                            $sort = "DESC" ?>
+                            <a id="sort_service_fee_DESC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
+                        <th scope="col">
+                            製作時間
+                            <?php $select = "production_time";
+                            $sort = "ASC" ?>
+                            <a id="sort_production_time_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "production_time";
+                            $sort = "DESC" ?>
+                            <a id="sort_production_time_DESC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
+                        <th scope="col">
+                            修改次數
+                            <?php $select = "modifications";
+                            $sort = "ASC" ?>
+                            <a id="sort_modifications_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "modifications";
+                            $sort = "DESC" ?>
+                            <a id="sort_modifications_DESC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
+                        <th scope="col">
+                            建立時間
+                            <?php $select = "create_at";
+                            $sort = "ASC" ?>
+                            <a id="sort_create_at_ASC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-down"></i></a>
+                            <?php $select = "create_at";
+                            $sort = "DESC" ?>
+                            <a id="sort_create_at_DESC" href="?page=<?= $page ?>&select=<?= $select ?>&sort=<?= $sort ?>"><i class="fas fa-arrow-alt-circle-up"></i></a>
+                        </th>
                         <th scope="col">修改</th>
                     </tr>
                 </thead>
@@ -78,7 +178,7 @@ if ($totalRows) {
                         <tr>
                             <td>
                                 <a href="javascript: del_it(<?= $r['ID'] ?>)">
-                                   刪除
+                                    <i class="fas fa-trash-alt"></i>
                                 </a>
                             </td>
                             <td><?= htmlentities($r['ID']) ?></td>
@@ -92,7 +192,7 @@ if ($totalRows) {
                             <td><?= htmlentities($r['create_at']) ?></td>
                             <td>
                                 <a href="Service_edit.php?ID=<?= $r['ID'] ?>">
-                                    修改
+                                    <i class="fas fa-edit"></i>
                                 </a>
                             </td>
                         </tr>
