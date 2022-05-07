64685062e0fa6a2bf3174d4be2167afcbd7d3929
Billy302 <101441065+Billy302@users.noreply.github.com>
2022-05-07 14:20:59 +0800

    clear

--- blog/parts/__scripts.php
+++
@@ -1,2 +0,0 @@
-<script src="./css/bootstrap/bootstrap.js"></script>
-<script src="https://kit.fontawesome.com/abacba9fba.js" crossorigin="anonymous"></script>
--- parts/scripts.php
+++
@@ -1,2 +0,0 @@
-<script src = "../Plugins/bootstrap-5.1.3/js/bootstrap.js"></script>
-<script src = "../Plugins/jQuery-3.6.0/jquery-3.6.0.min.js"></script>
