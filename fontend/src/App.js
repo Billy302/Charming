e65b317c3bd4b2fc743110eaab4ad20642723485
jessieChungQQ <99704866+jessieChungQQ@users.noreply.github.com>
2022-05-16 09:53:14 +0800

    新增CSS樣式

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -2,26 +2,36 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import {Routes, Route} from 'react-router-dom';
+
 //Pages
+import Home from './Account/Pages/Home';
+import SignIn from './Account/Pages/SignIn';
+import SignInIdentify from './Account/Pages/SignInIdentify';
+import SignInRecover from './Account/Pages/SignInRecover';
+import SignUp from './Account/Pages/SignUp';
+import SignUpInfo from './Account/Pages/SignUpInfo';
 
 //Components
+import Header from './Account/Components/Header';
+
 
 function App() {
   return (
-   <Container>
+    <Container>
      <Header />
      <Routes>
-      <Product
        {/* http://localhost:3000 */}
        <Route path="/" element={<Home />} />
-       {/* http://localhost:3000/about */}
-       <Route path="/about" element={<About />} />
-       {/* http://localhost:3000/contact */}
-       <Route path="/contact" element={<Contact />} />
-       {/* http://localhost:3000/categories */}
-       <Route path="/categories" element={<Category />} />
-       {/* http://localhost:3000/register */}
-       <Route path="/register" element={<Register />} />
+       {/* http://localhost:3000/signin */}
+       <Route path="/signin" element={<SignIn />} />
+       {/* http://localhost:3000/signin/identify */}
+       <Route path="/signin/identify" element={<SignInIdentify />} />
+       {/* http://localhost:3000/signin/recover */}
+       <Route path="/signin/recover" element={<SignInRecover />} />
+       {/* http://localhost:3000/signup */}
+       <Route path="/signup" element={<SignUp />} />
+       {/* http://localhost:3000/signup/info */}
+       <Route path="/signup/info" element={<SignUpInfo />} />
      </Routes>
   </Container>
   );
