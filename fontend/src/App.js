eac6887c53cef4e80badad78f8ada1e86ed134bc
ziting106 <99373797+ziting106@users.noreply.github.com>
2022-05-18 10:16:09 +0800

    20220518 --V1

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,29 +1,60 @@
+import React from "react";
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
-import {Routes, Route} from 'react-router-dom';
-//Pages
-
-//Components
+import Footer from './components/footer/Footer';
+import Blog from "./pages/Blog";
+import Communication from "./pages/Communication";
+import Login from "./pages/Login";
+import Users from "./pages/Users";
+import LoginHome from "./pages/loginHome/LoginHome";
+import Portfolio from "./pages/Portfolio";
+import Product from "./pages/Product"
+import ProductDetail from "./pages/ProductDetail"
+import ShoppingCar from "./pages/ShoppingCar"
+import UnloginHome from "./pages/unloginHome/UnloginHome"
+import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
 
 function App() {
   return (
-   <Container>
-     <Header />
-     <Routes>
-      <Product
-       {/* http://localhost:3000 */}
-       <Route path="/" element={<Home />} />
-       {/* http://localhost:3000/about */}
-       <Route path="/about" element={<About />} />
-       {/* http://localhost:3000/contact */}
-       <Route path="/contact" element={<Contact />} />
-       {/* http://localhost:3000/categories */}
-       <Route path="/categories" element={<Category />} />
-       {/* http://localhost:3000/register */}
-       <Route path="/register" element={<Register />} />
-     </Routes>
-  </Container>
+    <div className={App}>
+      <Router>
+        <Switch>
+          <Route path="/Blog">
+            <Blog />
+          </Route>
+          <Route path="/LoginHome">
+            <LoginHome />
+          </Route>
+          <Route path="/Communication">
+            <Communication />
+          </Route>
+          <Route path="/Product">
+            <Product />
+          </Route>
+          <Route path="/UnloginHome">
+            <UnloginHome />
+          </Route>
+          <Route path="/Portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/Login">
+            <LoginHome />
+          </Route>
+          <Route path="/LoginHome">
+            <LoginHome />
+          </Route>
+          <Route path="/ShoppingCar">
+            <ShoppingCar />
+          </Route>
+          <Route path="/Users">
+            <Users />
+          </Route>
+          <Route exact path="/">
+            <UnloginHome />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
