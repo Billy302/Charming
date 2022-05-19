8b6220578752d5d76fc6149eebf0bd7014f8cdab
ziting106 <99373797+ziting106@users.noreply.github.com>
2022-05-19 21:08:00 +0800

    home update

---
+++ fontend/src/Home/Pages/Portfolio.js
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+function Portfolio(){
+  return <div>Portfolio</div>
+}
+
+export default Portfolio
---
+++ fontend/src/Home/Pages/Product.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function Product(){
+  const params = useParams();
+  console.log(params);
+  return (
+    <div>
+      <h1>Product</h1>
+      <p>產品ＩＤ：{params.productId}</p>
+    </div>
+  );
+};
+
+export default Product;
