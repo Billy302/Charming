e6174f7925e145cc4459761a9293078775e9e77d
Billy302 <101441065+Billy302@users.noreply.github.com>
2022-05-20 10:23:33 +0800

    先將內容刪除

--- fontend/src/Sales/Pages/Cart/Cart3.js
+++ fontend/src/Sales/Pages/Cart/Cart3.js
@@ -1,34 +1,12 @@
-// reactjs-credit-card 引入失敗
-// 功能：新增訂單。Method: POST。URL: /api/order
-import React from "react";
-import Processbar from "../../Components/Processbar/Processbar";
+
+import React from 'react'
 
 function Cart3() {
+
   return (
     <>
-      <Processbar step="3" />
-      <h3>購物車-結帳 Page</h3>
-
-      <button
-        className="button"
-        onClick={() => {
-          // 到下一頁
-          // history.push('路徑')
-          // 新增訂單
-        }}
-      >
-        下一步
-      </button>
-      <button
-        className="button"
-        onClick={() => {
-          // 回首頁
-          // history.push('路徑')
-        }}
-      >
-        回上頁
-      </button>
+      <h3>購物車-結帳 Page(cart3)</h3>
     </>
-  );
+  )
 }
-export default Cart3;
+export default Cart3
