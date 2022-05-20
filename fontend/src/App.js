fcd4d78a9593f175409394c5df1958b857f3b03c
Billy302 <101441065+Billy302@users.noreply.github.com>
2022-05-20 09:07:32 +0800

    調整資料夾架構

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Cart1 from './Sales/Pages/Cart/Cart_1'
-import Cart2 from './Sales/Pages/Cart/Cart_2'
-import Cart3 from './Sales/Pages/Cart/Cart_3'
-import Cart4 from './Sales/Pages/Cart/Cart_4'
+import Cart1 from './Sales/Pages/Cart/Cart1'
+import Cart2 from './Sales/Pages/Cart/Cart2'
+import Cart3 from './Sales/Pages/Cart/Cart3'
+import Cart4 from './Sales/Pages/Cart/Cart4'
 
 function App() {
   return (
