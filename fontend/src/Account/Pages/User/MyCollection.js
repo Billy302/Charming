e31d39eb773371bcc1f43a96a967616971d9add2
jessieChungQQ <99704866+jessieChungQQ@users.noreply.github.com>
2022-05-25 14:19:29 +0800

    css樣式更新

--- fontend/src/Account/Pages/User/MyCollection.js
+++ fontend/src/Account/Pages/User/MyCollection.js
@@ -10,26 +10,40 @@ function MyCollection() {
       <BreadCrumb />
       {/* 上方選單 */}
       <nav className={style.navLeft}>
-        <Link to="/account">
+        <Link to="/account" className={style.active}>
           會員中心 <hr />
         </Link>
-        <Link to="/shoppinglist">
+        <Link to="/shoppinglist" className={style.unactive}>
           購買清單 <hr />
         </Link>
-        <Link to="/collection">
+        <Link to="/collection" className={style.unactive}>
           我的收藏 <hr />
         </Link>
-        <Link to="/notice">
+        <Link to="/notice" className={style.unactive}>
           我的通知 <hr />
         </Link>
       </nav>
 
-      {/* 內文 */}
-      <main>
-        <h1>我的收藏</h1>
+      {/* 右側內文 */}
+
+      <main className={style.main}>
+        <h1 className={style.h1}>我的收藏</h1>
       </main>
+
+      {/* 左側選單 */}
+      <aside className={style.aside}>
+        <ul className={style.ul}>
+          <li>
+            <Link to="/account">基本資料</Link>
+          </li>
+          <li>
+            <Link to="/notice/setting">通知設定</Link>
+          </li>
+        </ul>
+      </aside>
+      <div className={style.clear}></div>
     </>
-  )
+  );
 }
 
 export default MyCollection
