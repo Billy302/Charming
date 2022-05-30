a52702ebd07718ef5ee645a278c72e3dad47524c
Billy302 <101441065+Billy302@users.noreply.github.com>
2022-05-30 15:26:56 +0800

    新增測試路由

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Cart1 from './Sales/Pages/Cart/Cart1'
 import Cart2 from './Sales/Pages/Cart/Cart2'
 import Cart3 from './Sales/Pages/Cart/Cart3'
 import Cart4 from './Sales/Pages/Cart/Cart4'
 import MySale from './Sales/Pages/Order/MySale'
-
+import OrderList from './Sales/Pages/Order/OrderList'
 
 function App() {
   return (
-    <>
+    <div>
+      <Routes>
+        <Route path="/BtobPage/Order" element={<OrderList />} />
+      </Routes>
       {/* <Cart1 />
       <Cart2 />
       <Cart3 />
-      <Cart4 /> */}
-      <MySale />
-    </>
+      <Cart4 /> 
+       <MySale /> */}
+
+      
+    </div>
   )
 }
 
