0ecb5f7e7f62854f2d02b2777961f09b4a988d05
jessieChungQQ <99704866+jessieChungQQ@users.noreply.github.com>
2022-05-30 11:55:27 +0800

    註冊SQL輸入

--- backend/routes/Account/users.js
+++ backend/routes/Account/users.js
@@ -3,80 +3,40 @@ var router = express.Router();
 const app = express();
 const db = require('../../modules/mysql_config');
 
-// users demo data
-// const users = [
-//   {
-//     id: 1,
-//     username: 'harry',
-//     name: 'Harry Potter',
-//     password: '12345',
-//     role: 'user',
-//     address: {
-//       country: {
-//         city: 'London',
-//       },
-//     },
-//   },
-//   {
-//     id: 2,
-//     username: 'eddy',
-//     name:'Eddy',
-//     password: '33333',
-//     role: 'admin',
-//     address: {
-//       country: {
-//         city: 'New Taipei City',
-//       },
-//     },
-//   },
-// ]
-
-router
-  .route("/")
-  .get(async (req, res, next) => {
-    const sql = "SELECT * FROM users";
-    const [dates] = await db.query(sql);
-    res.json(dates);
-  })
+//此寫法與下相同 router.route("/").get(async (req, res, next) => 
+// router.get("/",async(req,res,next) => {
+//   const sql = "SELECT * FROM us_user";
+//   const [dates] = await db.query(sql);
+//   res.json(dates);
+// })
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   console.log(users)
-//   res.json(users);
-//     // res.send('respond with a resource');
-//   });
+router.get('/', function(req, res, next) {
+  // res.json(users);
+  res.send("<h1>後端正在監聽中</h1>");
+    // res.send('respond with a resource');
+  });
 
 // 登入
-router.post('/signin',function(req,res,next){
-  console.log(req.body);
-  res.send('登入');
+// router.post('/signin',function(req,res,next){
+//   console.log(req.body);
+//   // res.send('登入');
 // const {username, password} = req.body;
-// db.query(`SELECT * FROM users WHERE username='${username}' AND pwd='${password}'`,function(err,rs){if (err) throw err;
-//   console.log(rs);
-//   console.log('OK');
+// db.query(`SELECT * FROM us_user WHERE user_account='${username}' AND user_password='${password}'`,function(err,rows){
+//   if (err) throw err;
+//   console.log('Response:',rows);
+// });
 // })
-})
 
 // 註冊帳號密碼
-// router.post('/register',function(req, res,next){
-//   console.log(req.body);
-//   res.send("註冊成功");
-// })
+// sql=使用MySQL語法及欄位,VALUE值放表單name=""
+router.post('/register', async(req, res,next)=>{
+  console.log(req.body);
+  const sql = `INSERT INTO us_user(user_account,user_password,user_name,gender,birthday, email,mobile,city,interest_product,interest_collection,interest_article) VALUES ('${req.body.username}','${req.body.password}','${req.body.user_name}','${req.body.gender}','${req.body.birthday}','${req.body.email}','${req.body.mobile}','${req.body.city}','${req.body.interest_product}','${req.body.interest_collection}','${req.body.interest_article}')`;
+  const [user] = await db.query(sql)
+  res.json(user);
+})
 
-// 
-  // router.get('/:userid', function (req, res, next) {
-  //   const user = users.find((user) => String(user.id) === req.params.userid)
-  
-  //   if(user)
-  //     res.json(user)
-  //   else
-  //     res.json({message:"no user"})
-  // })
-
-
-
-
- 
  
 
   module.exports = router;
--- fontend/src/Account/Pages/SignIn/SignIn.js
+++ fontend/src/Account/Pages/SignIn/SignIn.js
@@ -7,34 +7,34 @@ import { FaEyeSlash, FaEye } from "react-icons/fa";
 import axios from "axios";
 
 
-function SignIn() {
-// function SignIn(props) {
-//   const [user_account, setUserAccount] = useState("");
-//   const [user_password, setUserPassword] = useState("");
-//   const { auth, setAuth } = props;
+// function SignIn() {
+function SignIn(props) {
+  const [user_account, setUserAccount] = useState("");
+  const [user_password, setUserPassword] = useState("");
+  const { auth, setAuth } = props;
 
-//   const login = (e) => {
-//     if (user_account !== "" && user_password !== "") {
-//       axios
-//         .post("http://localhost:3001/signin", {
-//           user_account: user_account,
-//           user_password: user_password,
-//         })
-//         .then((res) => {
-//           alert("登入成功!");
-//           navigate("/home");
-//         })
-//         .catch((e) => {
-//           if (e.response.error) {
-//             alert("帳號或密碼錯誤！");
-//           }
-//         });
-//     } else if (user_account === "") {
-//       alert("請輸入帳號!");
-//     } else {
-//       alert("請輸入密碼!");
-//     }
-//   };
+  const login = (e) => {
+    if (user_account !== "" && user_password !== "") {
+      axios
+        .post("http://localhost:3001/signin", {
+          user_account: user_account,
+          user_password: user_password,
+        })
+        .then((res) => {
+          alert("登入成功!");
+          navigate("/home");
+        })
+        .catch((e) => {
+          if (e.response.error) {
+            alert("帳號或密碼錯誤！");
+          }
+        });
+    } else if (user_account === "") {
+      alert("請輸入帳號!");
+    } else {
+      alert("請輸入密碼!");
+    }
+  };
   
   // useHistory版本已被useNavigate取代
   const navigate = useNavigate();
@@ -68,17 +68,17 @@ function SignIn() {
         {/* 登入 */}
         <form method="post" action="http://localhost:3001/Account/signin" className={style.form}>
           <label htmlFor="username">帳號</label>
-          <input id="username" type="text" name="username" />
+          <input id="username" type="text" name="user_account" />
            <label htmlFor='password'>密碼</label>
           <div className={style.password}>
             {/* 顯示/隱藏 密碼眼睛切換 text/password input type切換 */}
           {invisible &&  <FaEyeSlash onClick={invisibleHandler} />}
            {!invisible &&  <FaEye onClick={invisibleHandler} />}
           </div>
-          <input type={invisible? 'password' : 'text'} id="password" className={style.passwordShow} name="password"/>
+          <input type={invisible? 'password' : 'text'} id="password" className={style.passwordShow} name="user_password"/>
 
           <Link to="/signin/identify">忘記密碼</Link>
-        <button type="submit" className={style.button}>登入
+        <button onClick={login} className={style.button}>登入
         </button>
         </form>
             {/* onClick={() => {
