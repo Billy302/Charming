4bdbaf19a3736096882b5a2c4fa323f9a442854b
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2022-05-31 15:31:45 +0800

    article image update

--- fontend/src/Blog/Components/Article/ArticleHeader/ArticleHeader.js
+++ fontend/src/Blog/Components/Article/ArticleHeader/ArticleHeader.js
@@ -3,10 +3,14 @@ import article4 from './img/article4.jpg';
 import { useEffect } from 'react';
 
 const ArticleHeader = (props) => {
+    // const articleImage = require('../../../../../public/image/articleImage1.jpg');
     // useEffect(() => {
     //     console.log(props);
     // }, []);
     // 這邊程式碼按儲存後可以立即讀到console.log內容，但是如果重新更新網頁的話就會變成undefined,.
+    console.log(props.trendingArticle.article_image);
+    //
+    // console.log(articleImage);
     return (
         <header>
             <div className={classes['article--header']}>
@@ -15,7 +19,12 @@ const ArticleHeader = (props) => {
                     dangerouslySetInnerHTML={{ __html: props.trendingArticle.article_title }}
                 ></div>
                 <div className={classes['article--header__image']}>
-                    <img src={article4} alt=""></img>
+                    <img
+                        // src={require(`../../../../../public/image/${props.trendingArticle.article_image}.jpg`)}
+                        src={`http://localhost:3000/fontend/public/image/articleImage1.jpg`}
+                        // src={article4}
+                        alt=""
+                    ></img>
                 </div>
             </div>
         </header>
