7ff11e4429c23122d92f41381d773678dc19a297
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2022-06-03 11:53:39 +0800

    ArticleHeader is ready to go! this file is pretty simple and I think it's okay to commit it like this,

--- fontend/src/Blog/Components/Article/ArticleHeader/ArticleHeader.js
+++ fontend/src/Blog/Components/Article/ArticleHeader/ArticleHeader.js
@@ -3,12 +3,6 @@ import article4 from './img/article4.jpg';
 import { useEffect } from 'react';
 
 const ArticleHeader = (props) => {
-    // useEffect(() => {
-    //     console.log(props);
-    // }, []);
-    // 這邊程式碼按儲存後可以立即讀到console.log內容，但是如果重新更新網頁的話就會變成undefined,.
-    // 為什麼重新
-    // console.log(articleImage);
     return (
         <header>
             <div className={classes['article--header']}>
@@ -17,12 +11,7 @@ const ArticleHeader = (props) => {
                     dangerouslySetInnerHTML={{ __html: props.trendingArticle.article_title }}
                 ></div>
                 <div className={classes['article--header__image']}>
-                    <img
-                        // src={require(`../../../../../public/image/${props.trendingArticle.article_image}.jpg`)}
-                        src={`http://localhost:3000/image/${props.trendingArticle.article_image}.jpg`}
-                        // src={article4}
-                        alt=""
-                    ></img>
+                    <img src={`http://localhost:3000/image/${props.trendingArticle.article_image}.jpg`} alt=""></img>
                 </div>
             </div>
         </header>
