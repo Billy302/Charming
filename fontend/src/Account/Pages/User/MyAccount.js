448687f3b5c7b9627cfe8234313f4b656e66cbaa
jessieChungQQ <99704866+jessieChungQQ@users.noreply.github.com>
2022-06-05 17:43:51 +0800

    會員中心顯示資料ok

--- fontend/src/Account/Pages/User/MyAccount.js
+++ fontend/src/Account/Pages/User/MyAccount.js
@@ -24,11 +24,7 @@ useEffect(() => {
 
   // const { auth } = props;
 
-  {account.length > 0 && account.map((myaccount,i)=>{
-    const {id,user_account,user_password,user_name,gender,birthday,email,mobile,city,nickname,avatar,join_at} = myaccount;
-   
-     
-    })}
+
     return (
       <>
         <LoginNav />
@@ -48,26 +44,23 @@ useEffect(() => {
   
         {/* 右側內文 */}
   
-  
-  
-  
         <main className={style.main}>
           <h1 className={style.h1}>基本資料</h1>
           <div className={style.form}>
             <div className={style.item1}>
               <img src={require("../../images/Avatar3.png")} alt="male" />
-              {/* <label >Jacky Chen{user_name}</label> */}
+              <label >{account['user_name']}</label>
               {/* {auth ? "已登入" : "尚未登入"} */}
               <label>會員稱號：</label>
             </div>
   
             <div className={style.item2}>
-              {/* <label>暱稱</label> 
-              <label>男性</label>{gender}
-              <label>生日</label>{birthday}
-              <label>E-mail</label>{email}
-              <label>連絡電話</label>{mobile}
-              <label>所在地區</label>{city} */}
+              <label>暱稱</label> 
+              <label>男性</label>{account['gender']}
+              <label>生日</label>{account['birthday']}
+              <label>E-mail</label>{account['email']}
+              <label>連絡電話</label>{account['mobile']}
+              <label>所在地區</label>{account['city']}
             </div>
           </div>
           <div className={style.item3}>
