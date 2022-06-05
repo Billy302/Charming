e0a687a46e90451388b4a09ba960dd2b25e9bd9c
Billy302 <101441065+Billy302@users.noreply.github.com>
2022-06-06 00:42:18 +0800

    更新

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -7,13 +7,13 @@ import Footer from "./Home/Components/Footer/Footer";
 import PhoneFooter from "./Home/Components/PhoneFooter/PhoneFooter";
 
 //首頁
-import LoginHome from "./Home/Pages/LoginHome/LoginHome";
-import UnloginHome from "./Home/Pages/UnloginHome/UnloginHome";
-import MyProduct from "./Home/Pages/MyProduct/MyProduct";
-import EditProductPage from "./Home/Pages/EditProductPage/EditProductPage";
-import AddProduct from "./Home/Pages/AddProduct/AddProduct";
-import ProductPage from "./Home/Pages/ProductPage/ProductPage";
-import ProductPageEditButton from "./Home/Pages/ProductPageEditButton/ProductPageEditButton";
+import LoginHome from './Home/Pages/LoginHome/LoginHome'
+import UnloginHome from './Home/Pages/UnloginHome/UnloginHome'
+import MyProduct from './Home/Pages/MyProduct/MyProduct'
+import EditProductPage from './Home/Pages/EditProductPage/EditProductPage'
+import AddProduct from './Home/Pages/AddProduct/AddProduct'
+import ProductPage from './Home/Pages/ProductPage/ProductPage'
+import ProductPageEditButton from './Home/Pages/ProductPageEditButton/ProductPageEditButton'
 
 //會員
 //Pages
@@ -31,13 +31,13 @@ import MyAccount from "./Account/Pages/User/MyAccount";
 import MyNotice from "./Account/Pages/User/MyNotice";
 
 //購物車頁
-import Cart1 from "./Sales/Pages/Cart/Cart1";
-import Cart2 from "./Sales/Pages/Cart/Cart2";
-import Cart3 from "./Sales/Pages/Cart/Cart3";
-import Cart4 from "./Sales/Pages/Cart/Cart4";
-import MySale from "./Sales/Pages/Order/MySale";
-import Order from "./Sales/Pages/Order/Order";
-import OrderList from "./Sales/Pages/Order/OrderList";
+import Cart1 from './Sales/Pages/Cart/Cart1'
+import Cart2 from './Sales/Pages/Cart/Cart2'
+import Cart3 from './Sales/Pages/Cart/Cart3'
+import Cart4 from './Sales/Pages/Cart/Cart4'
+import MySale from './Sales/Pages/Order/MySale'
+import Order from './Sales/Pages/Order/Order'
+import OrderList from './Sales/Pages/Order/OrderList'
 
 //討論區
 // import AskPageOne from "./Forum/Pages/AskPageOne/AskPageOne";
@@ -59,9 +59,15 @@ function App() {
         {/* --------首頁------- */}
         <Route path="/" element={<UnloginHome />} />
         {/* 商品總覽頁 */}
-        <Route path="/Product/:UserId" element={<LoginHome />} />
+        {/* http://localhost:3000/Product?id=1&page=1&order=sell_count&sort=desc */}
+        <Route path="/Product" element={<LoginHome />} />
+        {/* http://localhost:3000/Product/1/13 */}
         <Route path="/Product/:UserId/:ProductID" element={<ProductPage />} />
-        <Route path="/MyProduct/:UserId" element={<MyProduct />} />
+        {/* http://localhost:3000/Product/13 */}
+        <Route path="/Product/:ProductID" element={<ProductPage />} />
+        {/* http://localhost:3000/MyProduct?id=1&page=1 */}
+        <Route path="/MyProduct" element={<MyProduct />} />
+        {/* http://localhost:3000/MyProduct/1/19 */}
         <Route
           path="/MyProduct/:UserId/:ProductID"
           element={<ProductPageEditButton />}
@@ -74,28 +80,30 @@ function App() {
         
         {/* --------註冊及登入----- */}
         {/* 忘記密碼 */}
-        <Route path="/signin/identify" element={<SignInIdentify />} />
+        {/* <Route path="/signin/identify" element={<SignInIdentify />} />
         <Route path="/signin/recover" element={<SignInRecover />} />
         {/* 基本資料 */}
-        <Route path="/signup/info" element={<SignUpInfo />} />
+        {/* <Route path="/signup/info" element={<SignUpInfo />} /> */}
         {/* 註冊 */}
-        <Route path="/signup" element={<SignUp />} />
+        {/* <Route path="/signup" element={<SignUp />} /> */}
         {/* 登入 */}
-        <Route path="/signin" element={<SignIn />} />
+        {/* <Route path="/signin" element={<SignIn />} /> */} 
         {/* -------user 登入後頁面------- */}
-        <Route path="/shoppinglist" element={<MyShoppingList />} />
+        {/* <Route path="/shoppinglist" element={<MyShoppingList />} />
         <Route path="/notice/setting" element={<NoticeSetting />} />
         <Route path="/collection" element={<MyCollection />} />
         <Route path="/account" element={<MyAccount />} />
-        <Route path="/notice" element={<MyNotice />} />
+        <Route path="/notice" element={<MyNotice />} /> */}
         {/* --------商品頁面-------- */}
-        <Route path="/Cart1" element={<Cart1 />} />
-        <Route path="/Cart2" element={<Cart2 />} />
-        <Route path="/Cart3" element={<Cart3 />} />
-        <Route path="/Cart4" element={<Cart4 />} />
-        <Route path="/MySale" element={<MySale />} />
-        <Route path="/Order" element={<Order />} />
-        <Route path="/OrderList" element={<OrderList />} />
+        <Route path="Sales/Cart1" element={<Cart1 />} />
+        <Route path="Sales/Cart2" element={<Cart2 />} />
+        <Route path="Sales/Cart3" element={<Cart3 />} />
+        <Route path="Sales/Cart4" element={<Cart4 />} />
+
+        <Route path="BtocPage/MySale" element={<MySale />} />
+        <Route path="BtobPage/Order/:id" element={<Order />} />
+        <Route path="BtobPage/Order" element={<OrderList />} />
+        
         {/* 討論區 */}
         {/* <Route path="/AskPageOne" element={<AskPageOne />} /> */}
         {/* <Route path="/AskPage" element={<AskPage />} />
