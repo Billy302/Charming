1a836ee8e9ade13d6b0a478e7f0f175f9f9ccffc
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2022-06-09 16:03:26 +0800

    banner 完成 上傳 banner 功能完成

--- fontend/src/Blog/Components/MyProductHeader/Banner.js
+++ fontend/src/Blog/Components/MyProductHeader/Banner.js
@@ -1,45 +1,97 @@
 import classes from './Banner.module.css'
-import image from './articleImage1.webp'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Trending from '../FrontPage/TrendingArticle/Trending'
+import PillBtn from '../UI/PillBtn'
 
 const Banner = (props) => {
   const [image, setImage] = useState({ preview: '', data: '' })
   const [status, setStatus] = useState('')
+  const [uploadBtn, setUploadBtn] = useState(false)
+  const [userBanner, setUserBanner] = useState([])
+  // render 追蹤的文章
+  const [allFavArticle, setAllFavArticle] = useState([])
 
+  const userId = localStorage.getItem('id')
+
+  const uploadBtnHandler = () => {
+    setUploadBtn(true)
+  }
+
+  // 上傳圖片
   const fileHandler = (e) => {
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
       data: e.target.files[0],
     }
     setImage(img)
-    console.log(e.target.files[0])
   }
+
   const imageUploadHandler = async (e) => {
     e.preventDefault()
     let formData = new FormData()
     formData.append('file', image.data)
-    const response = await fetch('http://localhost:3001/blog/image', {
-      method: 'POST',
-      body: formData,
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
+    const response = await fetch(
+      `http://localhost:3001/blog/image?userid=${userId}`,
+      {
+        method: 'POST',
+        body: formData,
+      }
+    )
     if (response) setStatus(response.statusText)
   }
+
+  //
+
+  // render user 的 banner
+
+  const fetchBanner = async () => {
+    const data = await fetch(
+      `http://localhost:3001/blog/image/render?userid=${userId}`
+    )
+    const result = await data.json()
+    setUserBanner(result[0])
+  }
+
+  useEffect(() => {
+    fetchBanner()
+  }, [])
+
+  console.log(userBanner)
+
+  // render user 追蹤的文章
+  useEffect(() => {
+    fetch(`http://localhost:3001/blog/fav/all?userid=${userId}`)
+      .then((res) => res.json())
+      .then((data) => setAllFavArticle(data))
+  }, [])
+
   return (
     <>
       <div className={classes['myproduct--banner']}>
-        <img src={image} alt="banner"></img>
+        {image.preview ? (
+          <img src={image.preview} width="100%" height="500" />
+        ) : (
+          <img
+            src={`http://localhost:3000/blog/upload/banner/${userBanner.banner_file}`}
+            alt="banner"
+            className={classes['myproduct--banner__upload']}
+          ></img>
+        )}
+        {uploadBtn && (
+          <form method="POST" onSubmit={imageUploadHandler}>
+            <input type="file" name="file" onChange={fileHandler} />
+            <button type="submit">upload</button>
+          </form>
+        )}
         <div className={classes['myproduct--banner__word']}>
-          <p>更換背景圖片</p>
+          <PillBtn onClick={uploadBtnHandler}>按我</PillBtn>
         </div>
       </div>
       {/* 圖片上傳 */}
       {/* <div>
         <h1>一刀殺進去！！</h1>
         {image.preview && (
-          <img src={image.preview} width="1000" height="1000" />
+          <img src={image.preview} width="100%" height="1000" />
         )}
         <form method="POST" onSubmit={imageUploadHandler}>
           <input type="file" name="banner" onChange={fileHandler} />
@@ -47,6 +99,10 @@ const Banner = (props) => {
         </form>
         {status && <h4>{status}</h4>}
       </div> */}
+      {/* user 追蹤的文章 */}
+      {allFavArticle.map((aritcle) => {
+        return <Trending article={aritcle} key={aritcle.fav_id} />
+      })}
     </>
   )
 }
