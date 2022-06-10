3a07c93c29c410a05af47e0ab5709376e4c1b8b2
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2022-06-10 12:09:55 +0800

    btoc 邏輯 完成b to c 渲染邏輯 可以讀取 user 追蹤的文章以及收藏的商品 按鈕也可以做文章以及商品的切換

--- fontend/src/Account/Pages/User/MyCollection.js
+++ fontend/src/Account/Pages/User/MyCollection.js
@@ -1,9 +1,32 @@
 import { Link } from 'react-router-dom'
-import style from "./User.module.css";
+import style from './User.module.css'
 import LoginNav from '../../../Home/Components/LoginNav/LoginNav'
 import BreadCrumb from '../../Components/BreadCrumb/BreadCrumb'
+import { useState, useEffect } from 'react'
+import Trending from '../../../Blog/Components/FrontPage/TrendingArticle/Trending'
+import MyLoveProduct from '../../../Home/Components/MyLikeProduct/MyLikeProduct'
+import MyCollectionButton from '../../Components/MyCollectionButton/MyCollectionButton'
+import { GiConsoleController } from 'react-icons/gi'
 
 function MyCollection() {
+  // -------- render 收藏文章功能 --------
+  // 紀錄所有收藏的文章
+  const [allFavArticle, setAllFavArticle] = useState([])
+  const userId = localStorage.getItem('id')
+  // render user 追蹤的文章
+  useEffect(() => {
+    fetch(`http://localhost:3001/blog/fav/all?userid=${userId}`)
+      .then((res) => res.json())
+      .then((data) => setAllFavArticle(data))
+  }, [])
+
+  // render 商品或文章功能
+  const [renderProduct, setRenderProduct] = useState(true)
+
+  const renderProductHandler = () => {
+    setRenderProduct(!renderProduct)
+  }
+
   return (
     <>
       <LoginNav />
@@ -18,18 +41,31 @@ function MyCollection() {
         </Link>
         <Link to="/collection" className={style.active}>
           我的收藏 <hr />
+          {/* user 追蹤的文章 */}
         </Link>
-
       </nav>
-
       {/* 右側內文 */}
-
-      <main className={style.main}>
-        <h1 className={style.h1}>我的收藏</h1>
-      </main>
-
+      <section className={style.dispalyFlex}>
+        <MyCollectionButton onRenderProduct={renderProductHandler} />
+        <main className={style.main}>
+          {renderProduct ? (
+            <MyLoveProduct />
+          ) : (
+            allFavArticle.map((aritcle) => {
+              return (
+                <Link
+                  to={`../blog/article/${aritcle.article_id}`}
+                  key={aritcle.article_id}
+                >
+                  <Trending article={aritcle} key={aritcle.fav_id} />
+                </Link>
+              )
+            })
+          )}
+        </main>
+      </section>
     </>
-  );
+  )
 }
 
 export default MyCollection
