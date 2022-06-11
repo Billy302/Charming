ec616f0b4cd71652dcd9d1a0a15ddca5a5ba7362
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2022-06-11 16:00:57 +0800

    banner 功能及css 完成 新增功能 1. 編輯封面照片變成一個按鈕 2. 上傳圖片後按鈕變成 '取消' '上傳'

--- fontend/src/Blog/Components/MyProductHeader/Banner.js
+++ fontend/src/Blog/Components/MyProductHeader/Banner.js
@@ -2,16 +2,13 @@ import classes from './Banner.module.css'
 import { useState, useEffect } from 'react'
 import Trending from '../FrontPage/TrendingArticle/Trending'
 import PillBtn from '../UI/PillBtn'
+import DefalutImage from './charmingDefalut.png'
 
 const Banner = (props) => {
   const [image, setImage] = useState({ preview: '', data: '' })
   const [status, setStatus] = useState('')
-  const [uploadBtn, setUploadBtn] = useState(false)
   const [userBanner, setUserBanner] = useState([])
-
-  const uploadBtnHandler = () => {
-    setUploadBtn(true)
-  }
+  const [displaySaveBtn, setDisplaySaveBtn] = useState(false)
 
   const userId = localStorage.getItem('id')
 
@@ -20,6 +17,16 @@ const Banner = (props) => {
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
       data: e.target.files[0],
+    }
+    setImage(img)
+    setDisplaySaveBtn(!displaySaveBtn)
+  }
+
+  const cancelUploadHandler = () => {
+    setDisplaySaveBtn(!displaySaveBtn)
+    const img = {
+      preview: '',
+      date: '',
     }
     setImage(img)
   }
@@ -51,36 +58,53 @@ const Banner = (props) => {
   useEffect(() => {
     fetchBanner()
   }, [])
-
-  // 上傳使用者小圖像
-
   return (
     <>
       <div className={classes['banner']}>
         {image.preview ? (
-          <img src={image.preview} width="100%" height="500" />
+          <img
+            src={image.preview}
+            width="100%"
+            height="500"
+            alt="user_upload"
+          />
         ) : (
           <img
-            src={`http://localhost:3000/blog/upload/banner/${userBanner.banner_file}`}
+            src={
+              userBanner
+                ? `http://localhost:3000/blog/upload/banner/${userBanner.banner_file}`
+                : DefalutImage
+            }
             alt="banner"
             className={classes['banner--uploadimage']}
           ></img>
         )}
         <div className={classes['banner--upload']}>
           <form id="banner-upload" method="POST" onSubmit={imageUploadHandler}>
-            <label
-              htmlFor="banner"
-              className={`${classes['custom-file-upload']}`}
-            >
-              編輯封面照片
-            </label>
+            {!displaySaveBtn ? (
+              <label
+                htmlFor="banner"
+                className={`${classes['custom-file-upload']}`}
+              >
+                編輯封面照片
+              </label>
+            ) : (
+              <div className={classes['banner--upload__cancel-submit']}>
+                <input
+                  type="button"
+                  value="取消"
+                  onClick={cancelUploadHandler}
+                />
+                <input type="submit" value="儲存變更" form="banner-upload" />
+              </div>
+            )}
+
             <input
               type="file"
               name="banner"
               id="banner"
               onChange={fileHandler}
             />
-            <input type="submit" value="儲存變更" form="banner-upload" />
           </form>
         </div>
       </div>
