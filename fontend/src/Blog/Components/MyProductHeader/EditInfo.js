3a9abf936eb6bba0b8e246221ba3151894af8847
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2022-06-11 16:05:06 +0800

    logo全部完成，personal info邏輯部分完成 logo新增功能 1. 一開始是上傳大頭照 2. 上傳成功之後變成取消以及儲存 personalinfo 修改內容 1. 改抬頭不改內容送出後內容不會被清空 2.改內容不改抬頭送出後內容不會被清空 3.什麼都不改送出兩個都不會被清空

--- fontend/src/Blog/Components/MyProductHeader/EditInfo.js
+++ fontend/src/Blog/Components/MyProductHeader/EditInfo.js
@@ -1,38 +1,86 @@
 import classes from './EditInfo.module.css'
 import PillBtn from '../UI/PillBtn'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const EditInfo = (props) => {
-  const [titleInput, setTitleInput] = useState('奔流設計')
+  // 解構從資料庫拿出來的使用者狀態的title跟context
+  const { userStatusData } = props
 
-  const [contextInput, setContextInput] = useState(
-    '《以海洋為主題的台灣設計師品牌》My Glück由Zoe以及Abraham Jr.兩位設計師組成，以充滿希望、關懷美麗的海洋為訴求，要讓更多人再次想起海的重要與美好。My Glück的設計系列包含從上衣、零錢包到居家抱枕等貼近日常生活的用品，希望這些色彩鮮豔、繽紛活潑的海洋生物能帶給這個世界更多的快樂，讓人們、海洋生物以及整個地球環境都能永續幸福。'
-  )
+  const titleUseRef = useRef('')
+  const contentUseRef = useRef('')
 
-  const titleChangeHandler = (e) => {
-    const setTitleInput = e.target.value
+  // 紀錄使用者更新的title context
+  // const [titleInput, setTitleInput] = useState('')
+  // const [contextInput, setContextInput] = useState('')
+  //紀錄user 更新的內容
+  // const titleChangeHandler = (e) => {
+  //   setTitleInput(e.target.value)
+  //   // 直接用useRef去找value並記下來，不然如果只改context沒有改title的話，送出時title會被洗掉
+  //   setContextInput(contentUseRef.current.value)
+  // }
+  // const contextChangeHandler = (e) => {
+  //   setContextInput(e.target.value)
+  //   // 直接用useRef去找value並記下來，不然如果只改title沒有改context的話，送出時context會被洗掉
+  //   setTitleInput(titleUseRef.current.value)
+  // }
+
+  // -------------更新 user title and content
+
+  // 找到目前的使用者id
+  const userId = localStorage.getItem('id')
+
+  // 串後端api並且更新使用者狀態
+  const updateStatus = async () => {
+    const data = await fetch(
+      `http://localhost:3001/blog/user/status/api?userid=${userId}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          titleInput: titleUseRef.current.value,
+          contextInput: contentUseRef.current.value,
+        }),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    const result = await data.json()
   }
-  const contextChangeHandler = (e) => {
-    const setContextInput = e.target.value
-  }
+
+  // 送出表格是要做的動作
   const inputChangeHandler = (e) => {
     e.preventDefault()
+    // 把使用者最新的更動記下來
+    const userNewestEdit = {
+      status_title: titleUseRef.current.value,
+      status_content: contentUseRef.current.value,
+    }
+    // 執行剛剛的api
+    updateStatus()
+    // 傳給 PersonalInfo ， PersonalInfo 再傳給 InfoDisplay component 讓他 render
+    props.onReRenderStatus(userNewestEdit)
+    // 告訴 PersonalInfo 使用者已經修改完成
+    props.onDisplay()
   }
+
   return (
     <div className={classes['personal-info--card__display']}>
-      <form onChange={inputChangeHandler}>
+      <form onSubmit={inputChangeHandler} id="test">
         <label htmlFor="title">修改文章</label>
         <input
           type="text"
           id="title"
-          onChange={titleChangeHandler}
-          defaultValue={titleInput}
+          // onChange={titleChangeHandler}
+          defaultValue={userStatusData[0].status_title}
+          ref={titleUseRef}
         ></input>
         <label htmlFor="context">修改內文</label>
         <textarea
           id="context"
-          onChange={contextChangeHandler}
-          defaultValue={contextInput}
+          // onChange={contextChangeHandler}
+          defaultValue={userStatusData[0].status_content}
+          ref={contentUseRef}
         ></textarea>
         <p></p>
         <PillBtn>完成修改</PillBtn>
--- fontend/src/Blog/Components/MyProductHeader/PersonalInfo.js
+++ fontend/src/Blog/Components/MyProductHeader/PersonalInfo.js
@@ -1,32 +1,168 @@
 import classes from './PersonalInfo.module.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import InfoDisplay from './InfoDisplay'
 import EditInfo from './EditInfo'
+import PillBtn from '../UI/PillBtn'
+import { FaCamera } from 'react-icons/fa'
 
 const PersonalInfo = (props) => {
+  // 判斷目前是要 render 修改文案(true)還是顯示文案(false)
   const [changeInfo, setChangeInfo] = useState(false)
-  const infoChangeHandler = () => {
+
+  // 取得目前 user id
+  const userId = localStorage.getItem('id')
+
+  // user logo 狀態區
+  const [image, setImage] = useState({ preview: '', data: '' })
+  const [status, setStatus] = useState('')
+  const [userLogo, setUserLogo] = useState('')
+
+  // render user status
+  const [editTitle, setEditTitle] = useState('')
+  const [contextTitle, setContextTitle] = useState('')
+  const [userStatus, setUserStatus] = useState([])
+
+  // ------------上傳user logo 邏輯
+
+  // 利用 createObjectURL api 來讓使用者預覽圖片
+  const fileHandler = (e) => {
+    const img = {
+      preview: URL.createObjectURL(e.target.files[0]),
+      data: e.target.files[0],
+    }
+    setImage(img)
+    setDisPlayCancelBtn(!displayCancelBtn) // 讓取消按鈕跑出來
+  }
+
+  // 把使用者上傳的 logo 傳到後端
+  const imageUploadHandler = async (e) => {
+    e.preventDefault()
+    let formData = new FormData()
+    formData.append('file', image.data)
+    const response = await fetch(
+      `http://localhost:3001/blog/logo?userid=${userId}`,
+      {
+        method: 'POST',
+        body: formData,
+      }
+    )
+    if (response) setStatus(response.statusText)
+  }
+
+  // ------------ render user logo 邏輯
+
+  // 到資料庫撈 user 目前的 logo
+  const fetchLogo = async () => {
+    const data = await fetch(
+      `http://localhost:3001/blog/logo/render?userid=${userId}`
+    )
+    const result = await data.json()
+    setUserLogo(result[0].logo_file)
+  }
+
+  useEffect(() => {
+    fetchLogo()
+  }, [])
+
+  // ------------ render user status
+
+  // render 使用者內文
+
+  const renderStatus = async () => {
+    const data = await fetch(
+      `http://localhost:3001/blog/user/renderStatus?userid=${userId}`
+    )
+    const result = await data.json()
+    setUserStatus(result)
+  }
+
+  useEffect(() => {
+    renderStatus()
+  }, [])
+
+  // 讓畫面變成修改使用者文案
+
+  const displayEditHandler = () => {
     setChangeInfo(true)
   }
-  // const displayChangeHandler = (e) => {
-  //     e.preventDefault();
-  //     setChangeInfo(false);
-  // };
+
+  const displayInfoHandler = () => {
+    setChangeInfo(false)
+  }
+
+  // 接 EditInfo 丟上來有關使用者更新的內容
+
+  const updateStatusRenderHandler = (update) => {
+    // console.log(update)
+    setUserStatus((prev) => [update, prev])
+  }
+
+  // 更新 '選擇大頭貼', '上傳' , '取消' 按鈕
+  const [displayCancelBtn, setDisPlayCancelBtn] = useState(false)
+  const displayCancelBtnHandler = () => {
+    setDisPlayCancelBtn(!displayCancelBtn)
+  }
+
+  const cancelUploadHandler = () => {
+    setDisPlayCancelBtn(!displayCancelBtn)
+    const img = {
+      preview: '',
+      date: '',
+    }
+    setImage(img)
+  }
 
   return (
     <div className={classes['personal-info']}>
       <div className={classes['personal-info--image']}>
-        <img
-          src={`http://localhost:3000/blog/upload/icon/developers5.png`}
-          alt="user logo"
-        ></img>
+        {image.preview ? (
+          <img src={image.preview} alt="user logo" />
+        ) : (
+          <img
+            src={`http://localhost:3000/blog/upload/icon/${userLogo}`}
+            alt="user logo"
+            className={classes['personal-info--image__user-upload']}
+          />
+        )}
+        <div className={classes['personal-info--upload-logo']}>
+          <form method="POST" onSubmit={imageUploadHandler} id="logo-form">
+            {!displayCancelBtn ? (
+              <label
+                htmlFor="logo"
+                className={`${classes['custom-file-upload']}`}
+              >
+                上傳大頭照
+              </label>
+            ) : (
+              <div
+                className={classes['personal-info--upload-logo__cancel-submit']}
+              >
+                <input
+                  type="button"
+                  onClick={cancelUploadHandler}
+                  value="取消"
+                />
+                <input type="submit" form="logo-form" value="上傳" />
+              </div>
+            )}
+            <input type="file" name="logo" id="logo" onChange={fileHandler} />
+          </form>
+        </div>
       </div>
+
       <div className={classes['personal-info--card']}>
         <div>
-          {!changeInfo ? (
-            <InfoDisplay onEdit={infoChangeHandler} />
+          {changeInfo ? (
+            <EditInfo
+              onDisplay={displayInfoHandler}
+              userStatusData={userStatus}
+              onReRenderStatus={updateStatusRenderHandler}
+            />
           ) : (
-            <EditInfo onDisplay={setChangeInfo} />
+            <InfoDisplay
+              onEdit={displayEditHandler}
+              userStatusData={userStatus}
+            />
           )}
         </div>
       </div>
